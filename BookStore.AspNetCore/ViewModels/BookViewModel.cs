98e3e8c335891bcf63a69fad69eea50e5af10acd
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-08-10 00:55:26 +0300

    Validations

---
+++ BookStore.AspNetCore/ViewModels/BookViewModel.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.AspNetCore.ViewModels
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "İsim alanı boş olamaz")]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int PageCount { get; set; }
+        [Required]
+        public string ImagePath { get; set; }
+        public DateTime PublishDate { get; set; }
+        [Required]
+        public string Author { get; set; }
+    }
+}
